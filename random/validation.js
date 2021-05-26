3422dbdc7be7838899b8a3c31bcb1f0dcec6a8ad
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-05-26 18:11:20 +0300

    small diferent changes

---
+++ random/validation.js
@@ -0,0 +1,9 @@
+class Validation {
+    method1 () {}
+    method2 () {}
+    method3 () {}
+}
+
+const v = new Validation()
+console.log(v.method1('Vardenis', true))
+console.log(v.method2('asmens kodas', false))
